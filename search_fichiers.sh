023e5971ee334e993d78a24f5dd050f6b09f4d6b
Eya Ben Khelifa <benkhelifa.eya@hotmail.fr>
2019-11-11 13:35:19 +0100

    Le readme nouvellement  modifié du projet MoSEF recherche de fichiers selon un deuxieme pattern

--- search_fichiers.sh
+++ search_fichiers.sh
@@ -2,10 +2,19 @@
 now=$(date +'%d %B %Y')
 echo Bienvenue  $USER
 
+# annonce le moment de l'execution
 echo -ne "Nous sommes le " $now "\n"
 
+# demande de fournir une chemin de repertoire
 echo -e "quel répertoire vous intéresse aujourd'hui ?"
 
 read directory
 
+# on affiche tous les fichiers dont le nom respecte un pattern fourni en paramètre (du répertoire saisi par l'utilisateur). 
+
 ls $directory/$1
+
+
+#On affiche uniquement les fichiers qui contiennent un deuxième pattern fourni en paramètre (de manière insensible à la casse).
+grep -il $2 $directory/$1
+
