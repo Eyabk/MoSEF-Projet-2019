ee2a2a958176baf4202c6264c4d70904e8346ac6
Eya Ben Khelifa <benkhelifa.eya@hotmail.fr>
2019-11-17 13:43:05 +0100

    ajout d'un dossier Screenshots et intégration de ces screenshots au readme et update du search_fichiers.sh avec plus de commentaires

--- search_fichiers.sh
+++ search_fichiers.sh
@@ -5,16 +5,15 @@ echo Bienvenue  $USER
 # annonce le moment de l'execution
 echo -ne "Nous sommes le " $now "\n"
 
-# demande de fournir une chemin de repertoire
+# demande de fournir un chemin de repertoire et affichage du répertoire fourni
 echo -e "quel répertoire vous intéresse aujourd'hui ?"
-
 read directory
 
-# on affiche tous les fichiers dont le nom respecte un pattern fourni en paramètre (du répertoire saisi par l'utilisateur). 
+# on affiche tous les fichiers dont le nom respecte un pattern fourni en paramètre du répertoire saisi par l'utilisateur. 
 
 ls $directory/$1
 
 
-#On affiche uniquement les fichiers qui contiennent un deuxième pattern fourni en paramètre (de manière insensible à la casse).
-grep -il $2 $directory/$1
+#On affiche uniquement les fichiers qui contiennent un deuxième pattern fourni en paramètre de manière insensible à la casse.
+grep -i $2 $directory/$1
 
